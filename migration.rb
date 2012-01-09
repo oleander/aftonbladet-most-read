0d24852a805e1e724c042e205b9f5e0f38f87220
Linus Oleander <linus@oleander.nu>
2012-01-09 14:52:05 +0100

    Adding migration

---
+++ migration.rb
@@ -0,0 +1,25 @@
+require "dm-migrations"
+require "dm-migrations/migration_runner"
+
+DataMapper.setup(:default, "sqlite:///#{File.expand_path File.dirname(__FILE__)}/db/database.sqlite3")
+DataMapper::Logger.new(STDOUT, :debug)
+DataMapper.logger.debug("Starting Migration")
+
+migration 1, :create_articles_table do
+  up do
+    create_table :articles do
+      column :id, Integer, serial: true
+      column :title, String
+      column :description, Text
+      column :image_url, String
+      column :token, Integer
+      column :published_at, DateTime
+    end
+  end
+
+  down do
+    drop_table :articles
+  end
+end
+
+migrate_up!
