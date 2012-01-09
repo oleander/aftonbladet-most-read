c5f9e2d9ab92cb02a2c94ccb50fd5afbbf9afac5
Linus Oleander <linus@oleander.nu>
2012-01-09 18:28:45 +0100

    Adding event loop to main thread

--- worker.rb
+++ worker.rb
@@ -14,14 +14,24 @@ require "./lib/config"
 require "./lib/aftonbladet"
 require "./lib/models"
 
-articles = Aftonbladet.new.articles
-ids = Article.all(token: articles.map(&:id)).map(&:token)
-
-articles.reject{|article| ids.include?(article.id)}.each do |article|
-  url    = B.shorten(article.url).short_url
-  length = 140 - url.length - 1
-  post   = StringUtils.truncate("#{article.title} – #{article.description}", length, "...") + " " + url
-  Article.create(token: article.id, published_at: article.published_at)  
-  Twitter.update(post)
-  abort
+sleep_time = 60*10 # 10 minutes
+loop do
+  # Fetching new ones from source
+  articles = Aftonbladet.new.articles
+  ids = Article.all(token: articles.map(&:id)).map(&:token)
+  
+  # Removing old ones
+  articles.reject!{|article| ids.include?(article.id)}
+  
+  puts "#{articles.count} articles found." if articles.any?
+  
+  articles.each do |article|
+    url    = B.shorten(article.url).short_url
+    length = 140 - url.length - 1
+    post   = StringUtils.truncate("#{article.title} – #{article.description}", length, "...") + " " + url
+    Article.create(token: article.id, published_at: article.published_at)  
+    Twitter.update(post)
+  end
+  
+  sleep(sleep_time)
 end
